eb55956149d2d46b45c10c4246c32674bd355604
Servando <swyswy214@gmail.com>
2021-11-21 15:50:24 -0600

    separate mongo and apollo

--- index.ts
+++ index.ts
@@ -1,30 +1,13 @@
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer } from "apollo-server";
 import { resolvers } from "./apollo/datasource/data-access/resolvers";
 import { typeDefs } from "./apollo/datasource/data-access/schema";
-import express from "express";
-import mongoose from "mongoose";
 
-const app = express();
 const port = process.env.port || 4000;
-const uri = `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@cluster0.faqfr.mongodb.net/${process.env.mongoDatabase}`;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-main().catch((err) => console.log(err));
-
-async function main() {
-  mongoose
-    .connect(uri)
-    .then(async () => {
-      await server.start();
-      server.applyMiddleware({ app });
-
-      app.listen({ port: port }, () => {
-        console.log(`Apollo server is running on http://localhost:${port}`);
-      });
-      console.log("Mongo server is up and running.");
-    })
-    .catch(() => console.log("Error while connecting to MongoDB"));
-}
+server.listen({ port: port }).then(({ url }) => {
+  console.log(`Apollo server running on ${url}`);
+});
--- mongo/server.ts
+++ mongo/server.ts
@@ -1,15 +1,14 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const uri = `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@cluster0.faqfr.mongodb.net/${process.env.mongoDatabase}`;
+const uri = `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@cluster0.faqfr.mongodb.net/${process.env.mongoDatabase}`;
 
-// main().catch((err) => console.log(err));
+main().catch((err) => console.log(err));
 
-// async function main() {
-//   mongoose
-//     .connect(uri)
-//     .then(() => {
-
-//       console.log("Mongo server is up and running.")
-//     })
-//     .catch(() => console.log("Error while connecting to MongoDB"));
-// }
+async function main() {
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("Mongo server is up and running.")
+    })
+    .catch(() => console.log("Error while connecting to MongoDB"));
+}
